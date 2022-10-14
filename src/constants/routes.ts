4a4172632a6906345cbde9136fba7ed3f034005b
kamta0905 <kamtap302@gmai.com>
2022-10-14 11:28:06 +0530

    navigation fixed

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,8 +1,8 @@
 import { RoutesParams } from "../Interfaces/RoutesParams"
 
 const routes:RoutesParams={
-    addReview:"/",
-    allReviews:"/all_review",
-    thanks:"/thanks"
+    addReview:"/React_F-G_KamtaPrasad",
+    allReviews:"/React_F-G_KamtaPrasad/all_review",
+    thanks:"/React_F-G_KamtaPrasad/thanks"
 }
 export default routes
